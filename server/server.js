06c98af58a558075ac1c18210a6f43b7a3f929fb
Davi <davidias0881@gmail.com>
2025-04-15 21:00:17 -0300

    My Network 2.1

--- server/server.js
+++ server/server.js
@@ -9,7 +9,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename); // Corrigido o nome da variável
 
 const app = express();
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 
 app.use(cors());
 app.use(express.json());
